ad7fe48ac5433ea3c7e8fc885128b8e6cbe8b12f
Dannenberger <kevdannenberg@gmail.com>
2021-01-14 12:44:49 -0500

    Added screen fragments and updated the action bar

--- app/src/main/java/com/example/chatter/MainActivity.kt
+++ app/src/main/java/com/example/chatter/MainActivity.kt
@@ -4,15 +4,38 @@ import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.example.chatter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val importContacts: Button = findViewById(R.id.button_import)
-        importContacts.setOnClickListener { importContacts() }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    /**
+     * Handles the creation of the ActionBar
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    /**
+     * Opens information about the app for the user.
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.about -> {
+                // TODO: Add action
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     /**
