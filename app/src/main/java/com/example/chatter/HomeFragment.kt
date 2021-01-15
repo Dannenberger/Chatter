844ffa0d2abec7b08f6f78f2d76e8f21fb904dcb
Dannenberger <kevdannenberg@gmail.com>
2021-01-15 09:39:13 -0500

    Added navigation to contact list fragment.

--- app/src/main/java/com/example/chatter/HomeFragment.kt
+++ app/src/main/java/com/example/chatter/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.chatter
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.chatter.databinding.FragmentHomeBinding
 
 /**
@@ -17,7 +20,26 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        setHasOptionsMenu(true)
+        binding.buttonManage.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_contactListFragment)
+        )
         return binding.root
+    }
+
+    /**
+     * Handles the creation of the action bar
+     */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    /**
+     * Handles actions taken when action bar items are selected
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 
 }
--- app/src/main/java/com/example/chatter/MainActivity.kt
+++ app/src/main/java/com/example/chatter/MainActivity.kt
@@ -4,38 +4,28 @@ import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.view.Menu
-import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.chatter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
     /**
-     * Handles the creation of the ActionBar
+     * Handles the up button functionality in the action bar
      */
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    /**
-     * Opens information about the app for the user.
-     */
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.about -> {
-                // TODO: Add action
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 
     /**
@@ -53,12 +43,12 @@ class MainActivity : AppCompatActivity() {
         )
         while (cursor != null && cursor.moveToNext()) {
             if (cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                val phoneNumber: String = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                if (!checkForValidNumber(phoneNumber)) continue
                 val id: String = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
                 val firstName: String = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME))
                 val lastName: String = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME))
                 val photoUri: String = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI))
-                val phoneNumber: String = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                if (!checkForValidNumber(phoneNumber)) continue
                 val contact = ContactModel(
                     id = id,
                     firstName = firstName,
